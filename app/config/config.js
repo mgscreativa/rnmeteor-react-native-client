723f5b34ab987e0f1674944aae4a44f0b94c7f8b
m.briglia <martin@mgscreativa.com>
2017-03-28 19:21:25 -0300

    Added fake locations and switch server buttons

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 export default {
   DEVELOPMENT_SERVER_URL: 'ws://10.0.0.2:3000/websocket',
-  PRODUCTION_SERVER_URL: 'ws://charging-stations.herokuapp.com/websocket',
+  PRODUCTION_SERVER_URL: 'ws://rnmeteor-meteor-backend-server.herokuapp.com/websocket',
   useProductionServer: true,
   errorStyles: {
     text: { color: '#fff' },
--- app/screens/FindNearMe.js
+++ app/screens/FindNearMe.js
@@ -1,8 +1,11 @@
 import Meteor from 'react-native-meteor';
 import React, { Component, PropTypes } from 'react';
+import { Card, Button } from 'react-native-elements';
+import { SecondaryButton } from '../components/Form';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
 import LocateMeButton from '../components/LocateMeButton';
+import colors from '../config/colors';
 import config from '../config/config';
 import Router from '../config/router';
 
@@ -18,6 +21,8 @@ class FindNearMe extends Component {
 
     this.state = {
       loading: false,
+      useProductionServer: (config.useProductionServer),
+      meteorServerName: (config.useProductionServer) ? 'development' : 'production',
     };
   }
 
@@ -50,7 +55,50 @@ class FindNearMe extends Component {
     );
   };
 
+  goToFakeNearMe = (latitude, longitude) => {
+    const position = {
+      coords: {
+        latitude: parseFloat(latitude),
+        longitude: parseFloat(longitude),
+      },
+    };
+
+    this.handleGeolocationSuccess(position);
+  };
+
+  toggleDDPServer = () => {
+    Meteor.disconnect();
+    const SERVER_URL = (!this.state.useProductionServer) ? config.PRODUCTION_SERVER_URL : config.DEVELOPMENT_SERVER_URL;
+    console.log('Connecting to :', SERVER_URL);
+    Meteor.connect(SERVER_URL);
+    this.setState({
+      useProductionServer: !this.state.useProductionServer,
+      meteorServerName: (!this.state.useProductionServer) ? 'development' : 'production',
+    });
+  };
+
   render() {
+    const locations = [
+      {
+        _id: '1',
+        name: 'MGS Creativa',
+        latitude: '-34.542',
+        longitude: '-58.541',
+      },
+      {
+        _id: '2',
+        name: 'Playtime',
+        latitude: '-34.670',
+        longitude: '-58.402',
+      },
+      {
+        _id: '3',
+        name: 'Google',
+        latitude: '37.422',
+        longitude: '-122.084',
+      }
+    ];
+
     return (
       <Container>
         <LocateMeButton
@@ -60,6 +108,25 @@ class FindNearMe extends Component {
         <Header>
           Find Nearest Charging Stations
         </Header>
+        <Card title="Use Fake Locations">
+          {
+            locations.map((location) => (
+              <Button
+                key={location._id}
+                raised
+                icon={{ name: 'my-location' }}
+                title={location.name}
+                backgroundColor={colors.primary}
+                buttonStyle={{ marginVertical: 5 }}
+                onPress={() => this.goToFakeNearMe(location.latitude, location.longitude)}
+              />
+            ))
+          }
+        </Card>
+        <SecondaryButton
+          title={`Switch to ${this.state.meteorServerName} Meteor server`}
+          onPress={this.toggleDDPServer}
+        />
       </Container>
     );
   }
